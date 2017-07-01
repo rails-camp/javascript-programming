98bc7c50d1187f4cfe5546143fe5cefc187fec63
Jordan Hudgens <jordan@devcamp.com>
2017-07-01 16:09:12 -0700

    Update section_b_12_type_casting.js

--- section_b_12_type_casting.js
+++ section_b_12_type_casting.js
@@ -1,3 +1,8 @@
+"100" + 42; // "10042"
+42 + "100"; // "42100"
+"100" - 42; // 58
+100 + null; // 100
+
 var ageOne = 12;
 String(ageOne); // '12'
 ageOne.toString(); // '12'
@@ -9,11 +14,7 @@ parseFloat(ageTwo); // 33
 + ageTwo; // 33
 parseInt('5555555555 is my phone number'); // 5555555555
 parseInt('foo 5555555555 is my phone number'); // NaN
+Number("100") + 42; // 142
 
 Number(true); // 1
 Number(false); // 0
-
-"100" + 42; // "10042"
-42 + "100"; // "42100"
-"100" - 42; // 58
-100 + null; // 100
