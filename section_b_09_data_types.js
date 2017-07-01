baa14d060158e0e12a4b5df5e826e6f15f5dba1e
Jordan Hudgens <jordan@devcamp.com>
2017-07-01 14:47:58 -0700

    Create section_b_09_data_types.js

---
+++ section_b_09_data_types.js
@@ -0,0 +1,19 @@
+// Boolean
+var truthy = true;
+var notTruthy = false;
+
+// Null
+var nully = null;
+
+// Undefined
+var notDefined;
+
+// Number
+var age = 12;
+
+// String
+var name = "Kristine";
+var nameTwo = 'Jordan';
+
+// Symbol
+var mySym = Symbol('foo');
