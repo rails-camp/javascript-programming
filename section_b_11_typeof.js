d230f7bfcfcb2a4d9fb04e0ff74351c3598ecf92
Jordan Hudgens <jordan@devcamp.com>
2017-07-01 15:33:16 -0700

    Create section_b_11_typeof.js

---
+++ section_b_11_typeof.js
@@ -0,0 +1,23 @@
+typeof 12;
+// "number"
+
+typeof 'Astros';
+// "string"
+
+typeof true;
+// "boolean"
+
+typeof { name: "Kristine" };
+// "object"
+
+var age;
+// undefined
+
+typeof age;
+// "undefined"
+
+age = null;
+// null
+
+typeof age;
+// "object"
