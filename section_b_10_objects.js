b2e0599d08bbc6136efa6cff2e48fb13b0fba0f4
Jordan Hudgens <jordan@devcamp.com>
2017-07-01 15:01:14 -0700

    Create section_b_10_objects.js

---
+++ section_b_10_objects.js
@@ -0,0 +1,12 @@
+var user = {
+  name: 'Kristine',
+  age: 12,
+  city: 'Scottsdale',
+  grades: {
+    math: 90,
+    science: 80,
+    languageArts: 100
+  }
+}
+user.age = 13;
+user.grades.coding = 95;
