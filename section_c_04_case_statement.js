e6a23ed1e9621ddc1f4cd1a5d67cbdd768a7e9d9
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 11:37:40 -0700

    Create section_c_04_case_statement.js

---
+++ section_c_04_case_statement.js
@@ -0,0 +1,15 @@
+var dataPoint = {};
+
+switch (typeof dataPoint) {
+  case "string":
+    console.log("It's a string");
+    break;
+  case "number":
+    console.log("It's a number");
+    break;
+  case "boolean":
+    console.log("It's a boolean");
+    break;
+  default:
+    console.log('No matches');
+}
