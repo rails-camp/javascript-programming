e8bfdfab6c679c46b06c9138687975ccc41e9cd6
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 11:22:55 -0700

    Update section_c_03_compound_conditionals.js

--- section_c_03_compound_conditionals.js
+++ section_c_03_compound_conditionals.js
@@ -1,15 +1,15 @@
 var age = 30;
 
 if (age <= 10) {
-  console.log('You can eat from the kid menu');
-  console.log('Not old enough to drive');
-  console.log('Not old enough to rent a car');
-} else if (age > 10 && age < 25) {
-  console.log('Adult menu time for you');
-  console.log('You can drive');
-  console.log('Not old enough to rent a car');
+  console.log("You can eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 16 && age < 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are not old enough to rent a car");
 } else if (age >= 25) {
-  console.log('Adult menu time for you');
-  console.log('You can drive');
-  console.log('You can rent a car');
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are old enough to rent a car");
 }
