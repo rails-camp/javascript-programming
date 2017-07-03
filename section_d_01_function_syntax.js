a6724619e0a610365e6aff7b23a47ac1cf4a9981
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 12:39:55 -0700

    Create section_d_01_function_syntax.js

---
+++ section_d_01_function_syntax.js
@@ -0,0 +1,19 @@
+function hiThere () {
+    console.log('Hi there');
+}
+
+hiThere(); // Hi there
+
+function hiThereTwo() {
+    return 'Hi there again';
+}
+
+hiThereTwo(); // "Hi there again"
+
+var storedText = hiThere(); // Hi there
+
+storedText; // undefined
+
+var storedTextTwo = hiThereTwo();
+
+storedTextTwo; // "Hi there again"
