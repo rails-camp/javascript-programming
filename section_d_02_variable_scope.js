602e4f146b7e7d2d6d5ea18da7efd95bba83c429
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 12:57:07 -0700

    Create section_d_02_variable_scope.js

---
+++ section_d_02_variable_scope.js
@@ -0,0 +1,15 @@
+userObj = {
+  email: 'sample@example.com',
+  fullName: 'Kristine Hudgens'
+}
+
+function dashboardGreeting() {
+  var userObj = {
+    email: 'sample2@example.com',
+    fullName: 'Jordan Hudgens'
+  }
+  console.log("Hi there, ".concat(userObj.fullName));
+}
+
+dashboardGreeting();
+console.log(userObj.fullName);
