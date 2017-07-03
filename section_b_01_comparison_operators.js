0c697c15c8a21ea3e596b4abfaea0ea792cf17d9
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 10:42:01 -0700

    Create section_b_01_comparison_operators.js

---
+++ section_b_01_comparison_operators.js
@@ -0,0 +1,18 @@
+var age = 10;
+var ageTwo = '12';
+
+if (age === ageTwo) {
+  console.log('They are equal');
+}
+
+if (age !== ageTwo) {
+  console.log('Not equal');
+}
+
+if (age >= 25) {
+  console.log('Old enough to rent a car');
+}
+
+if (age <= 10) {
+  console.log('You can eat from the kid menu');
+}
