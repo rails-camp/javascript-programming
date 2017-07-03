4548409ce963ef3f267d5184c794aefefc6e7e93
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 10:09:17 -0700

    Create section_b_18_order_of_operations.js

---
+++ section_b_18_order_of_operations.js
@@ -0,0 +1,13 @@
+/* Order of Operations
+   PEMDAS -> PEDMAS
+   Please Excuse My Dear Aunt Sally
+   Paranthesis
+   Exponents
+   Multiplication 5 / 5 * 4
+   Division
+   Addition
+   Subtraction
+*/
+
+var num = 5 + (5 * 10) / 6**2 - 1;
+var num = 5 / 5 * 4;
