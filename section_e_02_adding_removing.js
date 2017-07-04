6d4e69998d1d01ba4532f6b3f5681b64ea4e45d8
Jordan Hudgens <jordan@devcamp.com>
2017-07-04 10:56:33 -0700

    Create section_e_02_adding_removing.js

---
+++ section_e_02_adding_removing.js
@@ -0,0 +1,15 @@
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+arr.pop(); // "Springer"
+
+arr; // ['Altuve', 'Bregman', 'Correa']
+
+arr.push('Bagwell'); // 4
+
+arr; // ['Altuve', 'Bregman', 'Correa', 'Bagwell']
+
+arr.shift(); // ['Bregman', 'Correa', 'Bagwell']
+
+arr.unshift('Kyle'); // 4
+
+arr; // ['Kyle', 'Bregman', 'Correa', 'Bagwell']
