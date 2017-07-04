ca6cda9f3142b9ee0732f7b0fffee8b877c40660
Jordan Hudgens <jordan@devcamp.com>
2017-07-04 09:32:40 -0700

    Create section_d_09_this_keyword.js

---
+++ section_d_09_this_keyword.js
@@ -0,0 +1,20 @@
+var guide = {
+  title: 'Guide to Programming',
+  content: 'Content goes here...',
+  visibleToUser: function (viewingUserRole) {
+    if (viewingUserRole === 'paid') {
+      return true;
+    }
+  },
+  renderContent: function(userRole) {
+    if (this.visibleToUser(userRole)) {
+      console.log(this.title + " - " + this.content);
+    } else {
+      this.content = '';
+      console.log(this.title + " - " + this.content);
+    }
+  }
+}
+
+user = { role: 'paid' };
+guide.renderContent(user.role);
