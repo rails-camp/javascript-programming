179cd0b3441bc8b69a0770c0bc8c42f102325770
Jordan Hudgens <jordan@devcamp.com>
2017-07-04 12:40:30 -0700

    Create section_f_03_while_loops.js

---
+++ section_f_03_while_loops.js
@@ -0,0 +1,18 @@
+var players = [
+  'Altuve',
+  'Bregman',
+  'Correa',
+  'Springer'
+];
+
+var i = 0;
+while (i < players.length) {
+  console.log(players[i]);
+  i++;
+}
+
+var i = 21;
+do {
+  console.log(players[i]);
+  i++;
+} while (i < players.length)
