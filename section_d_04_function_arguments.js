7925390980f1996ebea974fa3426ecec4ace6ec7
Jordan Hudgens <jordan@devcamp.com>
2017-07-03 17:22:04 -0700

    Create section_d_04_function_arguments.js

---
+++ section_d_04_function_arguments.js
@@ -0,0 +1,6 @@
+function fullName(firstName, lastName, language) {
+    var language = language || 'English';
+    return lastName.toUpperCase() + ", " + firstName.toUpperCase() + " - " + language;
+}
+
+console.log(fullName('Jordan', 'Hudgens', 'Spanish'));
